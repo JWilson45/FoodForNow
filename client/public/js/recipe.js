e865d356776d5fa8425df80b7902464cf4866637
Phillip Belilovets <Phillip.dvoskin1@marist.edu>
2020-06-02 19:17:49 -0400

    fetch data form
    added a fetch function and a javascript file

---
+++ client/public/js/recipe.js
@@ -0,0 +1,27 @@
+const rName = document.getElementById('rName');
+const cTime = document.getElementById('cTime');
+const pTime = document.getElementById('pTime');
+const tTime = document.getElementById('tTime');
+const servings = document.getElementById('servings');
+const submit = document.getElementById('submit');
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+     body: JSON.stringify({
+    rName: ,
+    cTime: ,
+    pTime: ,
+    tTime: ,
+    servings: ,
+    submit: ,
+  })
+})
+.then(function (data) {
+  console.log('Request success: ', data);
+})
+.catch(function (error) {
+  console.log('Request failure: ', error);
+});
