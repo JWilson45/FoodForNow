f94443d6835a0b0066000de3bdc9461113f8d5ab
JWilson45 <jason.wilson2@marist.edu>
2020-06-02 19:38:15 -0400

    stage for DB update

--- server/dataAccess/getRecipe.js
+++ server/dataAccess/getRecipe.js
@@ -1,30 +1,52 @@
 const mysql = require('mysql');
 
+// Connection constraints for connection to recipeDB
+const con = mysql.createConnection({
+  host: "73.89.101.43",
+  user: "phil",
+  password: "phil1",
+  database: "recipeDB"
+});
 
 exports.getRecipe = function(req, res) {
   var id = req.body.id;
 
-var con = mysql.createConnection({
-  host: "192.168.1.140",
-  user: "phil",
-  password: "phil1"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("use recipeDB;", function(err, result) {
+  con.connect(function(err) {
     if (err) throw err;
-  });
-  con.query("SELECT ind.name, cf.quantity FROM Recipes r LEFT JOIN CallsFor cf " +
-  "ON r.recipeID = cf.recipeID LEFT JOIN Ingredients ind ON ind.IngredientsID = " +
-  "cf.IngredientsID WHERE cf.recipeID = "+ id +";",
+
+    // Query for selecting recipe based on its ID
+    con.query("SELECT ind.name, cf.quantity FROM Recipes r LEFT JOIN CallsFor cf " +
+    "ON r.recipeID = cf.recipeID LEFT JOIN Ingredients ind ON ind.IngredientsID = " +
+    "cf.IngredientsID WHERE cf.recipeID = "+ id +";",
     function(err, result) {
       if (err) throw err;
-      console.log(result);
+      // console.log(result);
+      //
+      // for (var i = 0; i < result.length; i++) {
+      //   console.log(result[i].name);
+      // }
+
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(id.toString());
+      res.status(200).send(result);
     })
   con.end();
 });
+}
+
+exports.inputRecipe = function (req, res) {
+  console.log('recieved');
+  console.log(req.body);
+
+  // con.connect(function(err) {
+  //   if (err) throw err;
+  //
+  //   con.query("INSERT INTO Ingredients (Name,Color,Catergory) VAULES " +
+  //   "('" + ingName + "', '" + color + "', '" + catergory + "');")
+  //
+  //   con.query("INSERT INTO Recipe (Name,Cooktime,Prep Time, Total Time, Servings, Directions) VAULES " +
+  //   "('" + recipeName + "', '" + cookTime + "', '" + catergory + "');")
+  // })
+
+
 
 }
