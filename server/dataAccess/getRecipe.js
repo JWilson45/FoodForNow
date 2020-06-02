326075591de58d5a3b7bd5a3b70b5d70b5fd5ea3
JWilson45 <jason.wilson2@marist.edu>
2020-06-02 17:21:09 -0400

    working on SLQ access

---
+++ server/dataAccess/getRecipe.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+
+
+exports.getRecipe = function(req, res) {
+  var id = req.body.id;
+
+var con = mysql.createConnection({
+  host: "192.168.1.140",
+  user: "phil",
+  password: "phil1"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("use recipeDB;", function(err, result) {
+    if (err) throw err;
+  });
+  con.query("SELECT ind.name, cf.quantity FROM Recipes r LEFT JOIN CallsFor cf " +
+  "ON r.recipeID = cf.recipeID LEFT JOIN Ingredients ind ON ind.IngredientsID = " +
+  "cf.IngredientsID WHERE cf.recipeID = "+ id +";",
+    function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(id);
+    })
+  con.end();
+});
+
+}
--- server/database-server.js
+++ server/database-server.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('./dataAccess/getRecipe');
 
 const app = express();
 const port = 3307;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send("What up. We in devolopment baby.")
 })
 
 app.get('/juan', (req, res) => {
+  console.log('Juan Sent');
   res.sendFile('juan.jpg', {
     root: './images'
   })
 })
 app.get('/jake', (req, res) => {
+  console.log('Jake Sent');
   res.sendFile('jake.jpg', {
     root: './images'
   })
 })
+
+app.route('/recipe').get(db.getRecipe)
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
