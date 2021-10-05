3fac968820bc843aad6252d0915945c4969ebcb2
Jake Tantorski <31451263+TantorskiJake@users.noreply.github.com>
2021-10-04 22:12:06 -0400

    Added models

---
+++ src/models/recipe.ts
@@ -0,0 +1,42 @@
+import { ObjectId } from "mongodb";
+
+class Recipe {
+  _id: ObjectId;
+  owner: ObjectId;
+  name: string;
+  alias: string;
+  description: string;
+  instructions: Array<string>;
+  ingredients: Array<Object>
+  isPublic: boolean;
+  dateCreated: Date
+  dateUpdated: Date;
+  type: string;
+  mealTime: string
+
+
+  constructor(_id: string, owner: string, name: string, alias: string,
+    description: string, instructions: Array<string>, ingredients: Array<Object>, 
+    isPublic: boolean,dateCreated: Date, dateUpdated: Date,
+    type: string, mealTime: string) {
+
+    // Create a new objectid object from a string... good for code reuse
+    this._id = new ObjectId(_id)
+    this.owner = new ObjectId(owner)
+    this.name = name
+    this.alias = alias
+    this.description = description
+    this.instructions = instructions
+    this.ingredients = ingredients
+    this.isPublic = isPublic
+    this.dateCreated = dateCreated
+    this.dateUpdated = dateUpdated
+    this.type = type
+    this.mealTime = mealTime
+  }
+
+}
+
+
+
+export default Recipe
