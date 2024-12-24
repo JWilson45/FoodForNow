5d157b4b7719876a964b169acefa4e7c66623377
Jason Wilson <35709473+JWilson45@users.noreply.github.com>
2024-12-24 12:38:37 -0500

    add 5MB limit

--- apps/api/src/utilities/barcode/multerConfig.js
+++ apps/api/src/utilities/barcode/multerConfig.js
@@ -36,6 +36,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
 });
 
 const uploadMiddleware = upload.single('barcodeImage');
